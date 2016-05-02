3d5e740ddb94fa43a833b6808477e5877ea219eb
Maxime Krauth <maximekm@gmail.com>
2016-05-02 19:18:09 +0200

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ActiveRecord::Base
   def stock_already_added?(ticker_symbol)
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock
-    user_stock.where(stock_id: stock.id).exists?
+    user_stocks.where(stock_id: stock.id).exists?
   end
 end
