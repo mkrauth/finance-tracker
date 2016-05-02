8b4634e1a3ce6ebeda522f573b00d60f41971704
Maxime Krauth <maximekm@gmail.com>
2016-05-02 10:50:18 +0200

    Finish AJAX

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -5,6 +5,10 @@ init_stock_lookup = function() {
         $('#stock-lookup').replaceWith(data);
         init_stock_lookup();
     })
+    $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+        $('#stock-lookup-results').replaceWith(' ');
+        $('#stock-lookup-errors').replaceWith('Stock was not found.');
+    });
 };
 
 
